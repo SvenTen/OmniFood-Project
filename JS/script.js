910fb864f35e54f0dee981b7908cee60ca21ff28
Jonathan <jonathan.ronnblad@outlook.com>
2023-09-07 14:55:46 +0300

    minor tweaks

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,7 @@ yearEl.textContent = currentYear;
 /* Make mobile navigation work */
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".main-nav");
+const headerEl = document.querySelector(".header");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
